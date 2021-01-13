4f74ef70132b3e1acee09c8bcfdcc44532e05aff
Gvidas <gvisis@gmail.com>
2021-01-13 02:46:46 +0200

    reaches the distance

--- src/CarRacing.java
+++ src/CarRacing.java
@@ -1,33 +1,43 @@
 public class CarRacing {
 
-    public static int carRandomMove(int number) {
-        int randomNumber = (int) (Math.random() * number + 1);
-        return randomNumber;
+    public static void sortCarDistancesDesc(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] < array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+            System.out.printf("Car distance: %d km\n", array[i]);
+        }
+    }
+
+    public static int randomMoveCarBy(int number) {
+        return (int) (Math.random() * number + 1);
     }
 
     public static void main(String[] args) {
         int[] cars = new int[8];
         int[] carTotalMove = new int[cars.length];
         int distanceToReach = 1000;
-        int currentDistance = 0;
 
-        while (currentDistance <= distanceToReach) {
-            int i;
-            for (i = 0; i < cars.length; i++) {
-                cars[i] = i + 1;
-                for (int j = 0; j < cars.length; j++) {
-                    cars[j] = j + 1;
-                    int carMoves = carRandomMove(10);
-                    carTotalMove[j] = carTotalMove[j] + carMoves;
-                    if (carTotalMove[j] >= distanceToReach){
-                        currentDistance = carTotalMove[j];
-                        for (int k = 0; k < cars.length; k++){
-                            System.out.printf("Car %d drove: %d km\n", k+1, carTotalMove[k]);
-                        }
-                        System.out.printf("Car %d won the game by reaching %d km first! \n", cars[j], currentDistance);
-                        return;
-                    }
+        while (true) {
+            int j;
+            for (j = 0; j < cars.length; j++) {
+                cars[j] = j + 1;
+                carTotalMove[j] = carTotalMove[j] + randomMoveCarBy(13);
+
+                // Check if car reached the finish line
+                if (carTotalMove[j] >= distanceToReach) {
+                    System.out.printf("Car %d won the game by reaching %d km first! \n", cars[j],
+                            carTotalMove[j]);
+
+                    // Sort car distances
+                    sortCarDistancesDesc(carTotalMove);
+                    return;
                 }
+
             }
         }
     }
@@ -35,12 +45,10 @@ public class CarRacing {
 
 // Kiekvieno ciklo metu turi pavaziuoti atstuma nuo 1-10 (random);
 // tada tikrinam ar kuri masina nuvaziavo 1000km ;
-// kai kazkuris pasiekia finiso linijos skaiciu, padeti ta skaiciu i kita int
-
-// EXTRA: jei nors viena masina pasiekia 100km ar 200km ir t.t. atspausdinti tarpinius rezultatus;
+// kai kazkuris pasiekia finiso linijos skaiciu, padeti ta skaiciu i kita int ?
 
 // Bent vienai masinai kirtus finiso linija ( pasiekus skaiciu 10 ir daugiau) baigiasi lenktynes
 // Baigus lenktynes, surusiuoti masinytes nuvaziuoto kelio MAZEJIMO tvarka.
 
-// kas 100km isspausdinti nuvaziuota atstuma;
+// EXTRA: jei nors viena masina pasiekia 100km ar 200km ir t.t. atspausdinti tarpinius rezultatus;
 
